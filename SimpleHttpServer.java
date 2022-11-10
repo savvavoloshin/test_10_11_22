b7e49b321c2083f1c8929266659b835f1b5d1a88
Savva Voloshin <savva.voloshin@gmail.com>
2022-11-10 17:04:46 +0300

    sqlite connection

--- SimpleHttpServer.java
+++ SimpleHttpServer.java
@@ -1,15 +1,26 @@
 // http://www.javenue.info/post/java-http-server
 
-import com.sun.net.httpserver.*;
-
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
+import com.sun.net.httpserver.Authenticator;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpPrincipal;
+import com.sun.net.httpserver.HttpServer;
+
+import netscape.javascript.JSObject;
+
+import org.json.JSONObject;
+
+import java.sql.Connection;  
+import java.sql.DriverManager;  
+import java.sql.SQLException;  
 
 public class SimpleHttpServer {
     public static void main(String[] args) throws Exception {
@@ -32,8 +43,37 @@ public class SimpleHttpServer {
                 System.out.println(exchange.getRequestMethod());
                 InputStream eis = exchange.getRequestBody();
 
-                String text = new BufferedReader(new InputStreamReader(eis, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
-                System.out.println(text);
+                String text = new String(eis.readAllBytes(), StandardCharsets.UTF_8);
+
+                JSONObject jo = new JSONObject(text);
+                
+                String name = jo.get("name").toString();
+                String password = jo.get("password").toString();
+
+                System.out.println(jo.toString());
+                System.out.println(password);
+
+                Connection conn = null;  
+                try {  
+                    // db parameters  
+                    String url = "jdbc:sqlite:users_and_messages.db";  
+                    // create a connection to the database  
+                    conn = DriverManager.getConnection(url);  
+                    
+                    System.out.println("Connection to SQLite has been established.");  
+                    
+                } catch (SQLException e) {  
+                    System.out.println(e.getMessage());  
+                } finally {  
+                    try {  
+                        if (conn != null) {  
+                            conn.close();  
+                        }  
+                    } catch (SQLException ex) {  
+                        System.out.println(ex.getMessage());  
+                    }  
+                } 
+
             }
 
             builder.append("<h1>URI: ").append(exchange.getRequestURI()).append("</h1>");
--- curl_test.sh
+++ curl_test.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
-curl -v -X POST http://localhost:8765 -H 'Content-Type: application/json' -d '{"login":"my_login","password":"my_password"}'
+curl -v -X POST http://localhost:8765 -H 'Content-Type: application/json' \
+ -d '{"name":"savva.voloshin","password":"123456"}'
